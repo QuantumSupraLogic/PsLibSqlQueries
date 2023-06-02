62ddb044a56eb6eac6f1e7795c80207d6336a07f
Lukas Szopo <lukas.szopo@denzelbank.at>
2023-06-02 10:55:26 +0200

    add auto delimiters

--- PsLibSqlQueries.psm1
+++ PsLibSqlQueries.psm1
@@ -1,8 +1,10 @@
+using module ./Private/AddDelimitersTransformAttribute.psm1
+
 #Get public and private function definition files.
 $Public  = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue )
 $Private = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue )
 #Dot source the files
-Foreach($import in @($Public + $Private))
+Foreach($import in @($Private + $Public))
 {
     Try
     {
